d75319bb826aca31240f9dacb2aae672cedc7e02
Minkyung99 <alsrud7412@naver.com>
2022-11-10 17:38:08 +0900

    2022-11-10

---
+++ 2022-11-10/js16-01.js
@@ -0,0 +1,3 @@
+const arr = [12,46,23,57,12,54,62];
+const average = arr.reduce((acc,cur,curidx) => {(acc+=cur)%index[arr]+0});
+console.log(average)
---
+++ 2022-11-10/js16-03.js
@@ -0,0 +1,9 @@
+const id1 = setTimeout(()=>console.log('1초 뒤에 실행됨'),1000)
+let count = 0;
+const id2 = setInterval(()=>{count++
+console.log(`[${count}번째 마다 실행됨`)},1000)
+
+setTimeout(()=>{
+    clearTimeout(id1)
+    clearInterval(id2)
+    },8000)
